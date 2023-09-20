804596ca331952fb89544a75eded18bb72d39804
marice-romero <marice.romero@gmail.com>
2023-09-19 21:47:45 -0700

    lesson 5-1 plus added functionality and css

--- src/components/AddTodoForm.js
+++ src/components/AddTodoForm.js
@@ -1,31 +1,67 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import InputWithLabel from "./InputWithLabel.js";
+import style from "./AddTodoForm.module.css";
 
 function AddTodoForm({ onAddTodo }) {
-  const [todoTitle, setTodoTitle] = useState("");
+  const [todo, setTodo] = useState({
+    title: "",
+    deadline: "",
+    completed: false,
+  });
 
-  function handleTitleChange(event) {
-    let newTodoTitle = event.target.value;
-    setTodoTitle(newTodoTitle);
+  function handleInputChange(event) {
+    setTodo({ ...todo, [event.target.name]: event.target.value });
   }
 
   function handleAddTodo(event) {
     event.preventDefault();
-    onAddTodo({ title: todoTitle });
-    setTodoTitle("");
+    onAddTodo(todo);
+
+    // setTodoTitle("");
+    setTodo({
+      title: "",
+      deadline: "",
+      completed: false,
+    });
   }
+
   return (
-    <form onSubmit={handleAddTodo}>
-      <InputWithLabel
-        isFocused
-        todoTitle={todoTitle}
-        handleTitleChange={handleTitleChange}
-      >
-        Title
-      </InputWithLabel>
-      <button type="submit">Add</button>
-    </form>
+    <div className={style.paper}>
+      <h1>add more sh*t to do</h1>
+      <div className={style.form}>
+        <div className={style.lines}></div>
+        <form onSubmit={handleAddTodo}>
+          <div className={style.formRow}>
+            <InputWithLabel
+              focus={true}
+              inputType={"text"}
+              todoType={"title"}
+              value={todo.title}
+              handleInputChange={handleInputChange}
+            >
+              title
+            </InputWithLabel>
+          </div>
+          <div className={style.formRow}>
+            <label htmlFor="deadline">deadline</label>
+            <input
+              type="date"
+              id="deadline"
+              name="deadline"
+              value={todo.deadline}
+              onChange={handleInputChange}
+              className={style.formInput}
+            ></input>
+          </div>
+          <div className="buttonContainer">
+            <button type="submit" className={style.submitButton}>
+              submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 }
 
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,9 +3,9 @@ import PropTypes from "prop-types";
 import TodoListItem from "./TodoListItem.js";
 import style from "./TodoList.module.css";
 
-function TodoList({ todoList, onRemoveTodo }) {
+function TodoList({ todoList, onRemoveTodo, onAddTodo, onEditTodo }) {
   return (
-    <>
+    <div className={style.listContainer}>
       <div className={style.lines}></div>
       <ul className={style.list}>
         {todoList.map((toDoItem) => {
@@ -14,17 +14,21 @@ function TodoList({ todoList, onRemoveTodo }) {
               key={toDoItem.id}
               toDoItem={toDoItem}
               onRemoveTodo={onRemoveTodo}
+              onAddTodo={onAddTodo}
+              onEditTodo={onEditTodo}
             />
           );
         })}
       </ul>
-    </>
+    </div>
   );
 }
 
 TodoList.propTypes = {
-  todoList: PropTypes.object,
+  todoList: PropTypes.array,
   onRemoveTodo: PropTypes.func,
+  onAddTodo: PropTypes.func,
+  onEditTodo: PropTypes.func,
 };
 
 export default TodoList;
